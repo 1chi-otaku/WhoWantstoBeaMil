64dc38276901d2fa01911f1e915dc72093383fb9
Serhii <mywaifuqueen@gmail.com>
2023-04-17 23:08:12 +0100

    Add project files.

---
+++ WhoWantstoBeaMil/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WhoWantstoBeaMil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 43);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Location = new System.Drawing.Point(74, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 43);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(151, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Стоп";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.ForeColor = System.Drawing.Color.Gold;
+            this.groupBox1.Location = new System.Drawing.Point(560, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 64);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подсказки";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.Location = new System.Drawing.Point(19, 15);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 43);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.Location = new System.Drawing.Point(143, 15);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 43);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.Location = new System.Drawing.Point(81, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 43);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(30, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(508, 87);
+            this.label1.TabIndex = 5;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button7.ForeColor = System.Drawing.Color.Gold;
+            this.button7.Location = new System.Drawing.Point(30, 413);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(247, 45);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button8.ForeColor = System.Drawing.Color.Gold;
+            this.button8.Location = new System.Drawing.Point(291, 413);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(247, 45);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button9.ForeColor = System.Drawing.Color.Gold;
+            this.button9.Location = new System.Drawing.Point(30, 485);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(247, 45);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button10.ForeColor = System.Drawing.Color.Gold;
+            this.button10.Location = new System.Drawing.Point(291, 485);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(247, 45);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(560, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 518);
+            this.label2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(793, 595);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.Name = "Form1";
+            this.Text = "Кто хочет стать миллионером?";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WhoWantstoBeaMil/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhoWantstoBeaMil
+{
+    public partial class Form1 : Form
+    {
+        private Label[] labels = new Label[15];
+        public Question[] questions= new Question[15];
+        public int current_question = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            int summ = 1000000;
+            for (int i = 0; i < 15; i++)
+            {
+                labels[i] = new Label();
+                labels[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                labels[i].Location = new System.Drawing.Point(566, 80+ i * 30);
+                labels[i].Name = "labels" + i;
+                labels[i].Size = new System.Drawing.Size(195, 26);
+                labels[i].TabIndex = i;
+                labels[i].Text = "" + (15 - i) + " - " + summ;
+                labels[i].Enabled = true;
+                Controls.Add(labels[i]);
+                labels[i].BringToFront();
+                summ /= 2;
+            }
+            labels[14-current_question].BackColor = Color.Green;
+            labels[14- current_question].ForeColor = Color.White;
+            labels[10].ForeColor = Color.Wheat;
+            labels[5].ForeColor = Color.Wheat;
+            labels[0].ForeColor = Color.Red;
+            InitQuestions();
+            LoadQuestion();
+        }
+        private void InitQuestions()
+        {
+            questions[0] = new Question("Как называется место на берегу, где обитают тюлени?", "Лежбище", "Стойбище", "Пастбище", "Гульбище","A");
+            questions[1] = new Question("Как мировая пресса называла премьер-министра Великобритании Маргарет Тэтчер?", "Стальная леди", "Железная леди", "Оловянный солдатик", "Крепкий орешек","B");
+            questions[2] = new Question("Какой из этих городов южнее остальных?", "Лондон", "Токио", "Мадрид", "Каир", "D");
+            questions[3] = new Question("Через какой город мира проходит нулевой меридиан? ", "Гринвич", "Гринсборо", "Глазго", "Гронинген","A");
+            questions[4] = new Question("Какая птица является символом Новой Зеландии?", "Киви", "Жако", "Эму", "Казуар", "A");
+            questions[5] = new Question("Какого короля англичане прозвали \"Львиное сердце\"?", "Вильгельм I", "Ричард I", "Карасик IV", "Генрих I", "B");
+            questions[6] = new Question("Как в народе называются финансовые институты, обещающие вкладчикам золотые горы?", "Пирамиды", "Гробницы", "Захоронения", "Сфинксы","A");
+            questions[7] = new Question("В каком году развалился СССР?", "1992", "1994", "1998", "1991", "D");
+            questions[8] = new Question("В каком городе родился Вольфганг Амадей Моцарт?", "Киев", "Прага", "Зальцбург", "Вена", "C");
+            questions[9] = new Question("Какую реку Юлий Цезарь перешел со словами \"Жребий брошен\"?", "Рубикон", "Припять", "Нил", "Евфрат","A");
+            questions[10] = new Question("Как называется искусство аранжировки цветов?", "Икебана", "Суши", "Кэндо", "Харакири", "A");
+            questions[11] = new Question("Какая страна является мировым лидером по производству кофе?", "Бразилия", "Венесуэла", "Украина", "Сан-Марино", "A");
+            questions[12] = new Question("Что труднее всего дается не трезвому человеку?", "Вязать Лыко", "Потрогать траву", "Бить баклуши", "Кушать голубику", "A");
+            questions[13] = new Question("Как называют японских мафиози?", "Джакузи", "Якуза", "Камиказдзе", "Коза Ностра", "A");
+            questions[14] = new Question("Участник какого из перечисленных спортивных состязаний экипирован винтовкой?", "Биатлон", "Бейсбол", "Футбол", "SMB", "A");
+        }
+
+        private void button7_Click(object sender, EventArgs e)//A
+        {
+            if (questions[current_question].correct_answer == "A")
+            {
+                AnswerHandler(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("You lost!");
+                button3_Click(sender, e);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)//B
+        {
+            if (questions[current_question].correct_answer == "B")
+            {
+                AnswerHandler(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("You lost!");
+                button3_Click(sender,e);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)//C
+        {
+            if (questions[current_question].correct_answer == "C")
+            {
+                AnswerHandler(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("You lost!");
+                button3_Click(sender, e);
+            }
+        }
+        private void button10_Click(object sender, EventArgs e)//D
+        {
+            if (questions[current_question].correct_answer == "D")
+            {
+                AnswerHandler(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("You lost!");
+                button3_Click(sender, e);
+            }
+        }
+        private void AnswerHandler(object sender, EventArgs e)
+        {
+            if (current_question == 14)
+            {
+                MessageBox.Show("You're a millionare!");
+                button1_Click(sender, e);
+            }
+            else
+            {
+                LabelColourHandler();
+                LoadQuestion();
+            }
+        }
+        private void LabelColourHandler()
+        {
+            if (current_question == 14) return;
+            labels[14 - current_question].BackColor = Color.Black;
+            labels[14 - current_question].ForeColor = Color.Gold;
+            current_question++;
+            labels[14 - current_question].BackColor = Color.Green;
+            labels[14 - current_question].ForeColor = Color.White;
+        }
+        private void LoadQuestion()
+        {
+            label1.Text = questions[current_question].question;
+            button7.Text = questions[current_question].answer_a;
+            button8.Text = questions[current_question].answer_b;
+            button9.Text = questions[current_question].answer_c;
+            button10.Text = questions[current_question].answer_d;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (questions[current_question].correct_answer == "A") MessageBox.Show("Зал склоняется к А и C!");
+            else if (questions[current_question].correct_answer == "B") MessageBox.Show("Зал склоняется к B и D!");
+            else if (questions[current_question].correct_answer == "C") MessageBox.Show("Зал склоняется к A и C!");
+            else if(questions[current_question].correct_answer != "D") MessageBox.Show("Зал склоняется к B и D!");
+            button6.Visible = false;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (questions[current_question].correct_answer != "A") button7.Text = "***";
+            else
+                button8.Text = "***";
+            if (questions[current_question].correct_answer != "C") button9.Text = "***";
+            else
+                button10.Text = "***";
+            button4.Visible = false;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (questions[current_question].correct_answer == "A") MessageBox.Show("Друг говорит - А!");
+            else if (questions[current_question].correct_answer == "B") MessageBox.Show("Друг говорит - B!");
+            else if (questions[current_question].correct_answer == "C") MessageBox.Show("Друг говорит - C!");
+            else if (questions[current_question].correct_answer != "D") MessageBox.Show("Друг говорит - D!");
+            button5.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            current_question = 0;
+            for (int i = 0; i < labels.Length; i++)
+            {
+                labels[i].BackColor = Color.Black;
+                labels[i].ForeColor = Color.Gold;
+            }
+            labels[10].ForeColor = Color.Wheat;
+            labels[5].ForeColor = Color.Wheat;
+            labels[0].ForeColor = Color.Red;
+            labels[14 - current_question].BackColor = Color.Green;
+            labels[14 - current_question].ForeColor = Color.White;
+            LoadQuestion();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int summ = 0;
+            if (current_question > 4) summ = 976;
+            if (current_question > 9) summ = 31250;
+            MessageBox.Show("You won " + summ);
+            button1_Click(sender, e);
+
+        }
+    }
+    public class Question
+    {
+        public string question { get; set; }
+        public string answer_a { get; set; }
+        public string answer_b { get; set;}
+        public string answer_c { get; set;}
+        public string answer_d { get; set;}
+        public string correct_answer { get; set;}
+        public Question(string question, string answer_a, string answer_b, string answer_c, string answer_d, string correct_answer)
+        {
+            this.question = question;
+            this.answer_a = answer_a;
+            this.answer_b = answer_b;
+            this.answer_c = answer_c;
+            this.answer_d = answer_d;
+            this.correct_answer = correct_answer;
+        }
+
+        public Question()
+        {
+        }
+    }
+}
